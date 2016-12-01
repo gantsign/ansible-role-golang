63aabf605d944ef00399107a90ae835a2adc5c75
John Freeman <john.freeman.opensource@gmail.com>
2016-12-01 22:10:59 +0000

    Updated Go to 1.7.4
    1.7.4 will now be installed by default.

--- tests/test_role.py
+++ tests/test_role.py
@@ -7,9 +7,9 @@ testinfra_hosts = AnsibleRunner('.molecule/ansible_inventory').get_hosts('all')
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.7.3$'),
+    ('GOROOT', '^/opt/go/1.7.4$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.7.3/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.7.4/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(Command, name, pattern):
