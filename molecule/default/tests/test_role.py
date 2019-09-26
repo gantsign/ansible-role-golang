6061498118f1890f00e637975b4569882229a74a
John Freeman <john.freeman.opensource@gmail.com>
2019-09-26 17:15:21 +0100

    Updated Go to 1.13.1
    1.13.1 will now be installed by default.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -9,9 +9,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.13$'),
+    ('GOROOT', '^/opt/go/1.13.1$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.13/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.13.1/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
