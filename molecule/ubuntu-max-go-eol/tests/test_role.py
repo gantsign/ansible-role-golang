cf81dc6a27a23e91beb7b239bd905a56d7ec326b
John Freeman <john.freeman.opensource@gmail.com>
2020-06-02 22:35:38 +0100

    Added support for Go 1.13.12 (#143)
    Go 1.14.4 remains the default version installed.

--- molecule/ubuntu-max-go-eol/tests/test_role.py
+++ molecule/ubuntu-max-go-eol/tests/test_role.py
@@ -9,9 +9,9 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.13.11$'),
+    ('GOROOT', '^/opt/go/1.13.12$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.13.11/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.13.12/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
