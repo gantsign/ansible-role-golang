8e259e72f630ecf5d4208f1fa8f56834f3d480bf
John Freeman <john.freeman.opensource@gmail.com>
2023-01-11 21:24:46 +0000

    Added support for Go 1.18.10
    Go 1.19.5 remains the default version installed.

--- molecule/ubuntu-max-go-eol/tests/test_role.py
+++ molecule/ubuntu-max-go-eol/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.18.9$'),
+    ('GOROOT', '^/opt/go/1.18.10$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.18.9/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.18.10/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
