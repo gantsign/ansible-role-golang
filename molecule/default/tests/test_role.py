63c1aef7de8b6e5f07cc7b5581adb1f6f19cf11f
John Freeman <john.freeman.opensource@gmail.com>
2023-01-11 20:46:11 +0000

    Updated Go to 1.19.5
    1.19.5 will now be installed by default.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.19.4$'),
+    ('GOROOT', '^/opt/go/1.19.5$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.19.4/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.19.5/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
