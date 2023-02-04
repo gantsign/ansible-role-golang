372d10e3a5f4de227c8eaa9a428d621260042980
John Freeman <john.freeman.opensource@gmail.com>
2023-02-04 09:59:48 +0000

    Updated Go to 1.20 (#274)
    1.20 will now be installed by default.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.19.5$'),
+    ('GOROOT', '^/opt/go/1.20$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.19.5/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.20/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
