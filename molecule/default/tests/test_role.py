9ea0134ab52e59d70633eebf4749f28c2dbff502
John Freeman <john.freeman.opensource@gmail.com>
2023-03-08 20:21:40 +0000

    Updated Go to 1.20.2
    1.20.2 will now be installed by default.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.20.1$'),
+    ('GOROOT', '^/opt/go/1.20.2$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.20.1/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.20.2/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
