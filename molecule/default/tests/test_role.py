05b5b883cc649aa2b6b36c5569c22d5e4150e780
John Freeman <john.freeman.opensource@gmail.com>
2024-11-16 10:55:55 +0000

    Updated Go to 1.23.3 (#392)
    1.23.3 will now be installed by default.
    
    Support for the following versions has also been added:
    * `1.23.2`
    * `1.23.1`
    * `1.23.0`
    * `1.22.9`
    * `1.22.8`

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.22.7$'),
+    ('GOROOT', '^/opt/go/1.23.3$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.22.7/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.23.3/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
--- molecule/ubuntu-max-go-eol/tests/test_role.py
+++ molecule/ubuntu-max-go-eol/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.21.13$'),
+    ('GOROOT', '^/opt/go/1.22.9$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.21.13/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.22.9/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
