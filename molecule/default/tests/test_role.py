4127e258ef93c09738fdc12494c5b0001438c07f
Philip Tamimi-Sarnikowski <philip@twig.energy>
2025-03-26 17:02:39 +0100

    add go 1.23.6,1.24.0,1.24.1

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -3,9 +3,9 @@ import re
 
 
 @pytest.mark.parametrize('name,pattern', [
-    ('GOROOT', '^/opt/go/1.23.5$'),
+    ('GOROOT', '^/opt/go/1.24.1$'),
     ('GOPATH', '^/root/workspace-go$'),
-    ('PATH', '^(.+:)?/opt/go/1.23.5/bin(:.+)?$'),
+    ('PATH', '^(.+:)?/opt/go/1.24.1/bin(:.+)?$'),
     ('PATH', '^(.+:)?/root/workspace-go/bin(:.+)?$')
 ])
 def test_go_env(host, name, pattern):
